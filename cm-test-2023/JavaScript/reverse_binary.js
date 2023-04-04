93c1bcaa2d399f4c3ba14e40390d9a63aacec505
unknown <matt.bedford@gmail.com>
2023-04-04 22:24:02 +0200

    Simplified JS, went all in on ES6

--- cm-test-2023/JavaScript/reverse_binary.js
+++ cm-test-2023/JavaScript/reverse_binary.js
@@ -1,30 +1,32 @@
 
-/*****
-* 
+/**
 * Function to reverse a binary number and provide also its corresponding integer value.
-* 
-*****/
+*/
 
-// Grab binary from a prompt
-var input = prompt("enter binary string");
+// Vars
+let input, result, message;
 
-// Remove whtespace
-input = input.replace(/\s/g, '');
+input = prompt("enter binary string", "101010");
+if(input) {
+    // Call main function
+    result = reverseBinary(input.toString());
 
-// Call main function. It returns either an error string or an object with reversedBinary and IntegerValue keys.
-// In either case, we log the result to console.
-let result = reverseBinary(input);
-if (typeof result === 'string') {
-    console.log(result);
-} else {
-    let x = `Reversed binary is ${result.reversedBinary} (integer value: ${result.integerValue})`;
-    console.log(x);
-}
+    // Expect result to be an object with two props else an error string
+    if (typeof result === 'string') {
+        message = result;
+    } else {
+        message = `Reversed binary is ${result.reversedBinary} (integer value: ${result.integerValue})`;
+    }
+    console.log(message);
+} 
 
 
 function reverseBinary(input) {
     // Validate input exists
     if(!input) return 'Error. You need to submit a value for this to work. Please try again.';
+
+    // Remove whitespace
+    input = input.replace(/\s/g, '');
     
     // Validate input is a valid binary string
     if(!checkIsABinary(input)) return 'Error. It seems that you didn\'t give us a legit binary string. Please try again.';
@@ -46,41 +48,17 @@ function reverseBinary(input) {
 
 
 function checkIsABinary(input) {
-    // Check for negatives, which could break our spread check, below.
-    // Store out orginal string in case this is so.
-    let neg = false;
-    let originalInput = input;
-    if(input.substring(0, 1) === '-') {
-        neg = true;
-        input = (input.substring(1));
-    }
-
-    // expand input with spread, then iterate over, looking for 1s and 0s only.
-    [...input].forEach(c => {
-        if(c !== 1 || c !== 0) return false;
-    });
-
-    // Return to negative if need be
-    if(!neg) input = originalInput;
-
-    // Check Ecmascript6 interpretation of binary is OK.
-    // We could actually have just done this without the spread above but I'm keeping the above
-    // as it might be useful if we needed to run this script on older browsers. 
-    let testBinary = '0b' + input;
+    // Thanks ECMAScript 6 for new 0b binary literal
+    let testBinary = `0b${input}`;
     if(isNaN(testBinary)) return false;
-
-    // If all those tests passed, we have a confirmed binary number
     return true;
 }
 
 function flipBinary(input) {
-    //Bit inversion on our binary
-    //return ~ input;
+    // Spread operator > array > flip > join
     return [...input].reverse().join('');
 }
 
 function getInteger(input) {
-    // Return input parsed as binary. Cuold also do "0b" trick, but I believe
-    // parseInt handles negatives better.
     return parseInt(input, 2);
 }
